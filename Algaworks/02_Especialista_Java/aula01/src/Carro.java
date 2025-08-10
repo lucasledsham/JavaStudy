9707ce5f0b7037111473effa084b981590710707
Lucas Ledsham <lucasnunesleal@gmail.com>
2025-08-10 13:18:59 -0300

    Novo Projeto Java

---
+++ Algaworks/02_Especialista_Java/aula01/src/Carro.java
@@ -0,0 +1,9 @@
+public class Carro {
+
+  int id;
+  String fabricante;
+  String modelo;
+  String cor;
+  int anoFabricacao;
+  Pessoa proprietario; // Composicao "tem um". Carro tem um proprietario
+}
