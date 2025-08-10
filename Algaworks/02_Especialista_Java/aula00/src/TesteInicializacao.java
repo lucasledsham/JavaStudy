1112dc9df96e3858b4cecd3c182a8225dfc47133
Lucas Ledsham <lucasnunesleal@gmail.com>
2025-08-10 12:09:26 -0300

    Aula Caindo a ficha - variáveis referenciam objetos

--- Algaworks/02_Especialista_Java/aula00/src/TesteInicializacao.java
+++ Algaworks/02_Especialista_Java/aula00/src/TesteInicializacao.java
@@ -14,5 +14,27 @@ public class TesteInicializacao {
     // Alterando proprietario
     carroTeste.proprietario = new Pessoa();
     System.out.println(carroTeste.proprietario.nome); // null
+
+    // Variaveis referenciam objetos
+    // Nao sao o objeto em si
+    // TESTES
+    System.out.println("\n");
+    System.out.println("=".repeat(95));
+    Pessoa pessoaTeste = new Pessoa();
+    pessoaTeste.nome = "Marcos";
+    pessoaTeste.anoNascimento = 2000;
+    pessoaTeste.cpf = "123";
+
+    Carro carroParaTestar = new Carro();
+    carroParaTestar.proprietario = pessoaTeste;
+
+    System.out.printf("Proprietario Nome: %s\nProprietario Ano Nascimento: %d\nProprietario CPF: %s", carroParaTestar.proprietario.nome, carroParaTestar.proprietario.anoNascimento, carroParaTestar.proprietario.cpf);
+
+    // O nome do proprietario do carro ira mudar para Julio
+    carroParaTestar.proprietario.nome = "Julio";
+    System.out.printf("\nProprietario Nome: %s\nProprietario Ano Nascimento: %d\nProprietario CPF: %s", carroParaTestar.proprietario.nome, carroParaTestar.proprietario.anoNascimento, carroParaTestar.proprietario.cpf);
+    System.out.println("\n");
+    System.out.printf("Nome objeto Pessoa: %s", pessoaTeste.nome);
+    System.out.printf("\nNome Proprietario Carro alterado (OBJETO UNICO!): %s", carroParaTestar.proprietario.nome);
   }
 }
